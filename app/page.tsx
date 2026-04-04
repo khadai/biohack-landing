6b62ecb49503c43b29abf56d70ed2bc72eb40b06
khadai <mykhailo.khadai@gmail.com>
2026-04-04 13:20:51 +0300

    fix icons

--- app/page.tsx
+++ app/page.tsx
@@ -4,24 +4,24 @@ export default function Home() {
     return (
         <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
             <main
-                className="flex flex-1 w-full max-w-3xl flex-col items-center  py-32 px-16 bg-white dark:bg-black sm:items-start">
+                className="flex flex-1 w-full max-w-3xl flex-col items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
                 <Image
                     className="dark:invert"
                     src="/logo.svg"
                     alt="Next.js logo"
-                    width={200}
-                    height={200}
+                    width={100}
+                    height={100}
                     priority
                 />
-                <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+                <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left pt-24">
                     <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
                         ІІ Учнівський біохакатон
                     </h1>
                     <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
                         Біотехнології після війни
-                        <br/><br/>
+                        <br/>
                         1 травня 2026
-                        <br/><br/>
+                        <br/>
                         Lviv Open Lab
                     </p>
                 </div>
