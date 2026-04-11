5e942af6b86fa70d6672acf9036852911592a248
khadai <mykhailo.khadai@gmail.com>
2026-04-11 12:31:01 +0300

    add link

--- app/components/site-footer.tsx
+++ app/components/site-footer.tsx
@@ -1,25 +1,41 @@
 import Image from "next/image";
 
 export function SiteFooter() {
-  return (
-    <>
-      <footer className="slogan">
-        <p>
-          Разом із університетами, STEM-хабами та біотех-компаніями будуємо
-          наукове майбутнє.
+    return (
+        <>
+            <footer className="slogan">
+                <p>
+                    Разом із університетами, STEM-хабами та біотех-компаніями будуємо
+                    наукове майбутнє.
+                </p>
+                <span>
+               <p style={{fontWeight: 500}}>
+          Email
         </p>
-        <span className="victory-text">Наука, що наближає Перемогу!</span>
-      </footer>
-      <div className="bottom-strip">
-        <span>© 2026 Біохакатон UA. Усі права захищені</span>
-        <Image
-          src="/logo.svg"
-          alt="Logo"
-          width={40}
-          height={56}
-          priority
-        />
-      </div>
-    </>
-  );
+                    <p>
+          <a href="mailto:biohackathon-ua@gmail.com">biohackathon-ua@gmail.com</a>
+        </p>
+          </span>
+                <span>
+               <p style={{fontWeight: 500}}>
+         Зв’язатися з нами
+        </p>
+                    <p>
+          <a href="tel:+380978236168">+38 097 823 6168</a>
+        </p>
+          </span>
+                <span className="victory-text">Наука, що наближає Перемогу!</span>
+            </footer>
+            <div className="bottom-strip">
+                <span>© 2026 Біохакатон UA. Усі права захищені</span>
+                <Image
+                    src="/logo.svg"
+                    alt="Logo"
+                    width={40}
+                    height={56}
+                    priority
+                />
+            </div>
+        </>
+    );
 }
--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -2,4 +2,4 @@ export const FORM_LINK = "https://forms.gle/zL73tnoRtotMggKP9";
 
 /** Event materials (Drive folder, PDFs). Override with NEXT_PUBLIC_EVENT_DOCS_URL. */
 export const EVENT_DOCS_LINK =
-  process.env.NEXT_PUBLIC_EVENT_DOCS_URL ?? "https://drive.google.com";
+    process.env.NEXT_PUBLIC_EVENT_DOCS_URL ?? "https://drive.google.com/file/d/18-e-x5rXrGRSOtQdohfOFNo98kVQ1OYm/view?usp=sharing";
