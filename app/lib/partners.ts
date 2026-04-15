30432c8d53477cefb4c95b7e29119bc83eb6d0ec
khadai <mykhailo.khadai@gmail.com>
2026-04-15 12:28:30 +0300

    add tvory logo

--- app/lib/partners.ts
+++ app/lib/partners.ts
@@ -4,6 +4,7 @@ import explogenLogo from "../assets/partners-logos/explogen.svg";
 import lnuLogo from "../assets/partners-logos/lnu.svg";
 import lvivdepLogo from "../assets/partners-logos/lvivdep.svg";
 import lvivOpenLabLogo from "../assets/partners-logos/lvivopenlab.svg";
+import tvoryLogo from "../assets/partners-logos/logo_tvory_ua.svg";
 import stLogo from "../assets/partners-logos/st.svg";
 import lab404 from "../assets/partners-logos/diy.svg";
 
@@ -13,6 +14,7 @@ export type PartnerLogo = {
 };
 
 export const partnerLogos: PartnerLogo[] = [
+  { src: tvoryLogo, alt: "Молодіжна організація Твори" },
   { src: lvivOpenLabLogo, alt: "Lviv Open Lab" },
   { src: lvivdepLogo, alt: "Львівська міська рада" },
   { src: lab404, alt: "DIY Лабораторія 404" },
